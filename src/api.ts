61f98fe95fccb6721ee9eb2405c033bedc094516
ykamoji <ykamoji@umass.edu>
2026-04-13 13:08:30 +0530

    update API_URL to use local development port and conditional production environment check

--- src/api.ts
+++ src/api.ts
@@ -1,1 +1,2 @@
-export const API_URL = `https://${process.env.NEXT_PUBLIC_API_URL}.onrender.com` || "http://localhost:8000"
+export const API_URL = process.env.NODE_ENV === "production" ?
+    `https://${process.env.NEXT_PUBLIC_API_URL}.onrender.com` : "http://127.0.0.1:5000/api"

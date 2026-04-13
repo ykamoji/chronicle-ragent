0fc55d835e245197514a066f42044bfee051cde8
ykamoji <ykamoji@umass.edu>
2026-04-13 21:40:20 +0530

    Production changes

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: '/:path*',
-        destination:
-          process.env.NEXT_PUBLIC_API_URL
-            ? `https://${process.env.NEXT_PUBLIC_API_URL}.onrender.com/api/:path*`
-            : 'http://127.0.0.1:5000/api/:path*',
-      },
-    ]
-  },
-  allowedDevOrigins: [
-    "http://localhost",
-    "192.168.1.5"
-  ],
-}
+const nextConfig = {}
 
 module.exports = nextConfig
